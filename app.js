4756ea937b1cd6627094172be7a58a1b5c21f448
Pradeep Kumar Tippa <pradeep_tippa@edgevere.com>
2017-03-06 19:40:31 +0530

    Working code of custom route service.

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var proxy = require('express-http-proxy');
-var app = express();
 var os = require('os');
- 
-app.all('*', proxy('https://node-consist-app.cfapps.io/', {
-    decorateRequest: function(proxyReq, originalReq) {
-        
-        console.log("originalReq url ", originalReq.url);
-        console.log("proxyReq headers ", proxyReq.headers);
-        originalReq.headers['Added-Proxy-2'] = 'express-http-proxy-2';
-        proxyReq.headers['Added-Proxy'] = 'express-http-proxy';
-        console.log("originalReq headers ", originalReq.headers);
-        return proxyReq;
-    }
-}));
- 
-app.listen(process.env.PORT || 3000);
+var HashRing = require('hashring');
+var request = require('request');
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    console.log("req. headers" , req.headers);
+    var url = req.headers['x-cf-forwarded-url'];
+    console.log("Redirecting url , ", url);
+    console.log("ENV \n", process.env);
+    var ring = new HashRing([
+            "6cab216e-49aa-4d8e-844b-72aabb880f4b:0", 
+            "6cab216e-49aa-4d8e-844b-72aabb880f4b:1"
+        ], 'md5', {
+            'max cache size': 10000
+        });
+    req.headers['X-Cf-App-Instance'] = ring.get(url);
+    req.pipe(request(url)).pipe(res);
+});
+server.listen(process.env.PORT || 3000);
